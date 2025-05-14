c440621dbe7286f0a478ccb9bf2c0273814c3287
Агафонова Анастасия <a.anastasi2005@mail.ru>
2025-05-14 21:27:04 +0700

    массив

---
+++ lab6/book.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Евгений Замятин',
+        Name: 'Мы',
+        publicationYear: 1925,
+        price: 7800,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Федор Достоевский',
+        Name: 'Белые ночи',
+        publicationYear: 1866,
+        price: 9830,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Нил Гейман',
+        Name: 'Американские боги',
+        publicationYear: 1957,
+        price: 9300,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Джек Лондон',
+        Name: 'Мартин Иден',
+        publicationYear: 1906,
+        price: 9305,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        Name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 1100,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Александр Куприн',
+        Name: 'Гранатовый браслет',
+        publicationYear: 1949,
+        price: 800,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Борис Пастернак',
+        Name: 'Доктор Живаго',
+        publicationYear: 1920,
+        price: 8360,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Михаил Шолохов',
+        Name: 'Тихий дон',
+        publicationYear: 1857,
+        price: 3840,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Эмили Бронте',
+        Name: 'Грозовой перевал',
+        publicationYear: 1899,
+        price: 3894,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Джон Толкин',
+        Name: 'Властелин колец',
+        publicationYear: 1954,
+        price: 2000,
+        bestseller: true,
+        available: true
+    }
+];
